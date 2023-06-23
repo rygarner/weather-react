b213ed28c1c082796707eacbc0fbe36ccbeff015
Ry <rgarn3@gmail.com>
2023-06-23 19:10:50 -0400

    updated icon url

--- src/Current-Weather.js
+++ src/Current-Weather.js
@@ -19,7 +19,7 @@ export default function CurrentWeather(props) {
       wind: response.data.wind.speed,
       city: response.data.name,
       description: response.data.weather[0].description,
-      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}`,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
